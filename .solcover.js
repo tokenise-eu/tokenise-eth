916b275a5755a183e7509082981e0f378051a725
Jules de Smit <julesdesmit@gmail.com>
2018-12-02 21:16:23 +0100

    Add testing coverage report

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+    compileCommand: '../node_modules/.bin/truffle compile',
+    testCommand: '../node_modules/.bin/truffle test --network coverage',
+    skipFiles: ['SecurityTokenInterface.sol'],
+}
